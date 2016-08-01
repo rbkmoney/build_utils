8863a934397750d434386df0172185f206c26141
Anton Belyaev <xantbel@gmail.com>
2016-08-01 19:07:48 +0300

    MSPF-75: fix branch meta (1)

--- examples/Dockerfile.sh
+++ examples/Dockerfile.sh
@@ -7,7 +7,7 @@ CMD /opt/dummy_service/bin/dummy_service.sh foreground
 LABEL base_image_version=$BASE_IMAGE_TAG
 LABEL build_image_version=$BUILD_IMAGE_TAG
 LABEL service_commit=$(git rev-parse HEAD)
-LABEL service_branch=$(git rev-parse --abbrev-ref HEAD)
+LABEL service_branch=$(git name-rev --name-only HEAD)
 LABEL service_commit_number=$(git rev-list --count HEAD)
 WORKDIR /opt
 EOF
