c4099adce0598346ed23ed6f61f6c0283553148b
Anton Belyaev <xantbel@gmail.com>
2016-08-17 17:03:41 +0400

    MSPF-86: namespace custom meta, add EXPOSE

--- examples/Dockerfile.sh
+++ examples/Dockerfile.sh
@@ -4,12 +4,13 @@ FROM $BASE_IMAGE
 MAINTAINER Rybek Rbkyev <r.rbkyev@rbkmoney.com>
 COPY examples/dummy_service.sh /opt/dummy_service/bin/dummy_service.sh
 CMD /opt/dummy_service/bin/dummy_service.sh foreground
-LABEL base_image_tag=$BASE_IMAGE_TAG
-LABEL build_image_tag=$BUILD_IMAGE_TAG
+EXPOSE 8022
+LABEL com.rbkmoney.base_image_tag=$BASE_IMAGE_TAG
+LABEL com.rbkmoney.build_image_tag=$BUILD_IMAGE_TAG
 # A bit of magic to get a proper branch name
 # even when the HEAD is detached (Hey Jenkins!
 # BRANCH_NAME is available in Jenkins env).
-LABEL branch=$( \
+LABEL com.rbkmoney.branch=$( \
   if [ "HEAD" != $(git rev-parse --abbrev-ref HEAD) ]; then \
     echo $(git rev-parse --abbrev-ref HEAD); \
   elif [ -n "$BRANCH_NAME" ]; then \
@@ -17,8 +18,8 @@ LABEL branch=$( \
   else \
     echo $(git name-rev --name-only HEAD); \
   fi)
-LABEL commit=$(git rev-parse HEAD)
-LABEL commit_number=$(git rev-list --count HEAD)
+LABEL com.rbkmoney.commit=$(git rev-parse HEAD)
+LABEL com.rbkmoney.commit_number=$(git rev-list --count HEAD)
 WORKDIR /opt
 EOF
 
