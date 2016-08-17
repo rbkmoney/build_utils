489afcebd55ca1817e41fb9fc366ad3c99e066bc
Anton Belyaev <xantbel@gmail.com>
2016-08-17 21:51:26 +0400

    MSPF-66: align LABELs with the new approach

--- examples/Dockerfile.sh
+++ examples/Dockerfile.sh
@@ -5,21 +5,20 @@ MAINTAINER Rybek Rbkyev <r.rbkyev@rbkmoney.com>
 COPY examples/dummy_service.sh /opt/dummy_service/bin/dummy_service.sh
 CMD /opt/dummy_service/bin/dummy_service.sh foreground
 EXPOSE 8022
-LABEL com.rbkmoney.base_image_tag=$BASE_IMAGE_TAG
-LABEL com.rbkmoney.build_image_tag=$BUILD_IMAGE_TAG
-# A bit of magic to get a proper branch name
+# A bit of magic below to get a proper branch name
 # even when the HEAD is detached (Hey Jenkins!
 # BRANCH_NAME is available in Jenkins env).
-LABEL com.rbkmoney.branch=$( \
-  if [ "HEAD" != $(git rev-parse --abbrev-ref HEAD) ]; then \
-    echo $(git rev-parse --abbrev-ref HEAD); \
-  elif [ -n "$BRANCH_NAME" ]; then \
-    echo $BRANCH_NAME; \
-  else \
-    echo $(git name-rev --name-only HEAD); \
-  fi)
-LABEL com.rbkmoney.commit=$(git rev-parse HEAD)
-LABEL com.rbkmoney.commit_number=$(git rev-list --count HEAD)
+LABEL com.rbkmoney.$SERVICE_NAME.parent=$BASE_IMAGE_NAME \
+      com.rbkmoney.$SERVICE_NAME.parent_tag=$BASE_IMAGE_TAG \
+      com.rbkmoney.$SERVICE_NAME.commit_id=$(git rev-parse HEAD) \
+      com.rbkmoney.$SERVICE_NAME.commit_num=$(git rev-list --count HEAD) \
+      com.rbkmoney.$SERVICE_NAME.branch=$( \
+        if [ "HEAD" != $(git rev-parse --abbrev-ref HEAD) ]; then \
+          echo $(git rev-parse --abbrev-ref HEAD); \
+        elif [ -n "$BRANCH_NAME" ]; then \
+          echo $BRANCH_NAME; \
+        else \
+          echo $(git name-rev --name-only HEAD); \
+        fi)
 WORKDIR /opt
 EOF
-
