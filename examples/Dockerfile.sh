464d6e47310966f8af7b43f2166ef930a9fe6c30
Anton Belyaev <xantbel@gmail.com>
2016-09-05 14:47:59 +0400

    CAPI-25: Meta fix: commit_num -> commit_number

--- examples/Dockerfile.sh
+++ examples/Dockerfile.sh
@@ -13,7 +13,7 @@ LABEL com.rbkmoney.$SERVICE_NAME.parent=$BASE_IMAGE_NAME \
       com.rbkmoney.$SERVICE_NAME.build_img=build \
       com.rbkmoney.$SERVICE_NAME.build_img_tag=$BUILD_IMAGE_TAG \
       com.rbkmoney.$SERVICE_NAME.commit_id=$(git rev-parse HEAD) \
-      com.rbkmoney.$SERVICE_NAME.commit_num=$(git rev-list --count HEAD) \
+      com.rbkmoney.$SERVICE_NAME.commit_number=$(git rev-list --count HEAD) \
       com.rbkmoney.$SERVICE_NAME.branch=$( \
         if [ "HEAD" != $(git rev-parse --abbrev-ref HEAD) ]; then \
           echo $(git rev-parse --abbrev-ref HEAD); \
