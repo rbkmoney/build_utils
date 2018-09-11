0f4556488482bbc580172d5272de7f700b84d5c4
Grigory Antsiferov <azr@bakka.su>
2018-09-11 12:06:07 +0300

    Make repo shallow depth configurable (REPO_SHALLOW_SINCE)

--- sh/repo-init.sh
+++ sh/repo-init.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-test -n "${UTILS_PATH}" || exit 2
+if [ -z "${UTILS_PATH}" ]; then
+    echo "Env UTILS_PATH is not defined" >&2
+    exit 2
+fi
+set -u
 source "${UTILS_PATH}/sh/functions.sh"
 
 d_repo="$1"
 remote_uri="$2"
+shallow_since="${3:-$(date "+%Y-%m-%d" -d "2 weeks ago")}"
 
 if [[ "${remote_uri}" == "git+ssh"* ]]; then
     export GIT_SSH_COMMAND="$(which ssh) -o StrictHostKeyChecking=no -o User=git $([ -n "${SSH_PRIVKEY}" ] && echo -o IdentityFile="${SSH_PRIVKEY}")"
@@ -11,8 +16,8 @@ if [[ "${remote_uri}" == "git+ssh"* ]]; then
 fi
 if [[ -d "${d_repo}/.git" ]]; then
     einfo "Syncing repository ${d_repo}"
-    git -C "${d_repo}" checkout -q master || exit $?
-    git -C "${d_repo}" pull -q || exit $?
+    git -C "${d_repo}" fetch -q origin master --shallow-since="${shallow_since}" || exit $?
+    git -C "${d_repo}" checkout -fq master || exit $?
 else
     einfo "Initialising repository ${d_repo}"
     if [[ -d "${d_repo}" ]]; then
@@ -24,6 +29,6 @@ else
     mkdir -p "${d_repo}"
     eend $? "Failed to create directory ${d_repo}" || exit $?
     einfo "Clonning repository ${d_repo}"
-    git clone -q  --shallow-since="$(date "+%Y-%m-%d" -d "2 weeks ago")" "${remote_uri}" "${d_repo}" || exit $?
+    git clone -q --shallow-since="${shallow_since}" "${remote_uri}" "${d_repo}" || exit $?
 fi
 
