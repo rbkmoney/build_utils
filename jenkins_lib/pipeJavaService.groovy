269686d735abef363f9f40a1bf4e1b7c751f3722
Pospolita Nikita <nikita7asics@gmail.com>
2018-12-10 15:54:30 +0300

    Java service pipeline actualization (#48)

--- jenkins_lib/pipeJavaService.groovy
+++ jenkins_lib/pipeJavaService.groovy
@@ -1,64 +1,22 @@
 // Default pipeline for Java service
-def call(String serviceName, String baseImageTag, String buildImageTag, String dbHostName, String mvnArgs = "") {
+def call(String serviceName, Boolean useJava11 = false, String mvnArgs = "") {
     // service name - usually equals artifactId
     env.SERVICE_NAME = serviceName
-    // service java image tag
-    env.BASE_IMAGE_TAG = baseImageTag
-    // build container image tag
-    env.BUILD_IMAGE_TAG = buildImageTag
-    // data base name
-    env.DB_NAME = serviceName
-    // host url for database. If null - DB will not start
-    env.DB_HOST_NAME = dbHostName
-    // mvnArgs - arguments for mvn install in build container. For exmple: ' -DjvmArgs="-Xmx256m" '
+    // use java11 or use std JAVA_HOME (java8)
+    env.JAVA_HOME = useJava11 ? "JAVA_HOME=/opt/openjdk-bin-11.0.1_p13 " : ""
 
-    // Using withRegistry() for auth on docker hub server.
-    // Pull it to local images with short name and reopen it with full name, to exclude double naming problem
-    def buildContainer = docker.image('rbkmoney/build:$BUILD_IMAGE_TAG')
-    runStage('Pull build image') {
-        docker.withRegistry('https://dr.rbkmoney.com/v2/', 'dockerhub-rbkmoneycibot') {
-            buildContainer.pull()
-        }
-        buildContainer = docker.image('dr.rbkmoney.com/rbkmoney/build:$BUILD_IMAGE_TAG')
-    }
+    // mvnArgs - arguments for mvn. For example: ' -DjvmArgs="-Xmx256m" '
 
-    def postgresImage;
-    try {
-        // Start db if necessary.
-
-        def insideParams = '';
-        if (dbHostName != null) {
-            runStage('Run PostgresDB container') {
-                postgresImage = docker.image('dr.rbkmoney.com/rbkmoney/postgres:9.6').run(
-                        '-e POSTGRES_PASSWORD=postgres ' +
-                                '-e POSTGRES_USER=postgres ' +
-                                '-e POSTGRES_DB=$DB_NAME '
-                )
-                insideParams = ' --link ' + postgresImage.id + ':$DB_HOST_NAME '
-            }
-        }
-        // Run mvn and generate docker file
-        runStage('Execute build container') {
-            withCredentials([[$class: 'FileBinding', credentialsId: 'java-maven-settings.xml', variable: 'SETTINGS_XML']]) {
-                buildContainer.inside(insideParams) {
-                    def mvn_command_arguments = ' --batch-mode --settings  $SETTINGS_XML -P ci ' +
-                            '-Ddockerfile.base.service.tag=$BASE_IMAGE_TAG ' +
-                            '-Ddockerfile.build.container.tag=$BUILD_IMAGE_TAG ' +
-                            '-Ddb.url.host.name=$DB_HOST_NAME ' +
-                            " ${mvnArgs}"
-                    if (env.BRANCH_NAME == 'master') {
-                        sh 'mvn deploy' + mvn_command_arguments
-                    } else {
-                        sh 'mvn package' + mvn_command_arguments
-                    }
-                }
-            }
-        }
-    }
-    finally {
-        if (postgresImage != null) {
-            runStage('Stop PostgresDB container') {
-                postgresImage.stop()
+    // Run mvn and generate docker file
+    runStage('Maven package') {
+        withCredentials([[$class: 'FileBinding', credentialsId: 'java-maven-settings.xml', variable: 'SETTINGS_XML']]) {
+            def mvn_command_arguments = ' --batch-mode --settings  $SETTINGS_XML -P ci ' +
+                    " -Dgit.branch=${env.BRANCH_NAME} " +
+                    " ${mvnArgs}"
+            if (env.BRANCH_NAME == 'master') {
+                sh env.JAVA_HOME + 'mvn deploy' + mvn_command_arguments
+            } else {
+                sh env.JAVA_HOME + 'mvn package' + mvn_command_arguments
             }
         }
     }
@@ -74,7 +32,7 @@ def call(String serviceName, String baseImageTag, String buildImageTag, String d
         if (env.BRANCH_NAME == 'master') {
             runStage('Push Service image') {
                 docker.withRegistry('https://dr.rbkmoney.com/v2/', 'dockerhub-rbkmoneycibot') {
-                    serviceImage.push();
+                    serviceImage.push()
                 }
                 // Push under 'withRegistry' generates 2d record with 'long name' in local docker registry.
                 // Untag the long-name
