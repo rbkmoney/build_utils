bb94edd7a6bd0a560236d4455fd7e869d0481584
Alexey Pronin <24279065+vulnbe@users.noreply.github.com>
2019-01-18 16:58:29 +0300

    Add ability to set SERVICE_IMAGE_NAME

--- make_lib/utils_common.mk
+++ make_lib/utils_common.mk
@@ -16,8 +16,8 @@ export GIT_SSH_COMMAND
 endif
 
 REGISTRY ?= dr.rbkmoney.com
-ORG_NAME := rbkmoney
-SERVICE_IMAGE_NAME := $(REGISTRY)/$(ORG_NAME)/$(SERVICE_NAME)
+ORG_NAME ?= rbkmoney
+SERVICE_IMAGE_NAME ?= $(REGISTRY)/$(ORG_NAME)/$(SERVICE_NAME)
 export SERVICE_NAME
 
 SHELL := /bin/bash
