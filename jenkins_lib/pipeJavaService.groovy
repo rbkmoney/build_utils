c2991197dd4cfad2b7e86447c7aa488b0b08a430
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-01-18 14:39:15 +0300

    Add sonar

--- jenkins_lib/pipeJavaService.groovy
+++ jenkins_lib/pipeJavaService.groovy
@@ -21,6 +21,22 @@ def call(String serviceName, Boolean useJava11 = false, String mvnArgs = "") {
         }
     }
 
+    // Send result build to sonar
+    runStage('Maven sonar') {
+        withCredentials([[$class: 'FileBinding', credentialsId: 'java-maven-settings.xml', variable: 'SETTINGS_XML']]) {
+            def mvn_command_arguments = ' --batch-mode --settings  $SETTINGS_XML -P ci ' +
+                    " -Dgit.branch=${env.BRANCH_NAME} " +
+                    " ${mvnArgs}"
+            try {
+                sh env.JAVA_HOME + 'mvn sonar:sonar' + mvn_command_arguments +
+                        " -Dsonar.host.url=${env.SONAR_ENDPOINT}"
+            }
+            catch (Exception e) {
+                echo "Can't send to sonar ${env.SONAR_ENDPOINT}."
+            }
+        }
+    }
+
     def serviceImage;
     def imgShortName = 'rbkmoney/' + env.SERVICE_NAME + ':' + '$COMMIT_ID';
     getCommitId()
