a1e7521f3d769183d85d3587199c4787323218df
Alexey Pronin <a@vuln.be>
2019-03-19 16:28:23 +0300

    Add env var

--- jenkins_lib/pipeDefault.groovy
+++ jenkins_lib/pipeDefault.groovy
@@ -1,4 +1,6 @@
 def call(Closure body, String registry = "dr.rbkmoney.com", String registryCredentialsId = "dockerhub-rbkmoneycibot") {
+  env.REGISTRY = registry
+
   runStage('init pipeline') {
     docker.withRegistry('https://' + registry + '/v2/', registryCredentialsId) {
       body.call()
