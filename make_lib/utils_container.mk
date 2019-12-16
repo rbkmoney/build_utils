defd326a18f10c016b21982c0a422a788c69a945
amalgamm <amalgamm@users.noreply.github.com>
2019-12-16 15:54:11 +0300

    Update utils_container.mk

--- make_lib/utils_container.mk
+++ make_lib/utils_container.mk
@@ -66,7 +66,7 @@ to_wdeps_shell: gen_compose_file
 	{ \
 	echo "Warning: 'make wc_shell' is the preferred way to run dev environment." ; \
 	$(DOCKER_COMPOSE) up -d ; \
-	$(if $(DOCKER_COMPOSE_PREEXEC _HOOK),$(DOCKER_COMPOSE_PREEXEC_HOOK);,) \
+	$(if $(DOCKER_COMPOSE_PREEXEC_HOOK),$(DOCKER_COMPOSE_PREEXEC_HOOK);,) \
 	$(DOCKER_COMPOSE) exec $(SERVICE_NAME) $(DOCKER_RUN_CMD) $(UNAME) $(GNAME) ; \
 	$(DOCKER_COMPOSE) kill ; \
 	$(DOCKER_COMPOSE) down ; \
