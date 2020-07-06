d7b2534ae9033df1dfa664b62d80e9a2c7cdcc3f
Sergey Yelin <elinsn@gmail.com>
2020-07-06 09:33:37 +0300

    Harden otp version filename (avoid clashes)

--- jenkins_lib/withDialyzerCache.groovy
+++ jenkins_lib/withDialyzerCache.groovy
@@ -1,11 +1,19 @@
 def call(Closure body) {
     def withWsCache = load("${env.JENKINS_LIB}/withWsCache.groovy")
     env.SH_TOOLS = "build_utils/sh"
-    String OtpReleaseFN = "otp_version.tmp"
+    String OtpReleaseFN = "otp_version." + suffix()
     sh "make wc_cmd WC_CMD='${env.SH_TOOLS}/otp_version ${OtpReleaseFN}'"
     String OtpRelease = readFile OtpReleaseFN
     def Plt = ["_build/default/rebar3_",OtpRelease,"_plt"].join("")
     withWsCache(Plt, body)
+}
+
+static def suffix() {
+    def pool = ['a'..'z','A'..'Z',0..9].flatten()
+    Random rand = new Random(System.currentTimeMillis())
+
+    def passChars = (0..6).collect { pool[rand.nextInt(pool.size())] }
+    passChars.join()
 }
 
 return this;
