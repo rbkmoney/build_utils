2c661595fc2975561719a88e084d5fa3dfed4266
Sergey Yelin <elinsn@gmail.com>
2020-07-06 14:01:16 +0300

    Change erlang pipe signature

--- jenkins_lib/pipeErlangService.groovy
+++ jenkins_lib/pipeErlangService.groovy
@@ -1,5 +1,5 @@
 // Default pipeline for Erlang services
-def call(boolean testWithDependencies = false, boolean runInParallel = false, Closure body) {
+def runPipe(boolean testWithDependencies = false, boolean runInParallel = false) {
     withPrivateRegistry() {
         if (masterlikeBranch()) {
             // RELEASE pipe
