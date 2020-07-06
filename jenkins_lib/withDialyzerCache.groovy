91c84b2d7f8fc94711f9fa5c2029497776d4d105
Sergey Yelin <elinsn@gmail.com>
2020-07-06 19:21:02 +0300

    Move script dir setup to Jenkinsfile

--- jenkins_lib/withDialyzerCache.groovy
+++ jenkins_lib/withDialyzerCache.groovy
@@ -1,6 +1,5 @@
 def call(Closure body) {
     def withWsCache = load("${env.JENKINS_LIB}/withWsCache.groovy")
-    env.SH_TOOLS = "build_utils/sh"
     String OtpReleaseFN = "otp_version." + suffix()
     sh "make wc_cmd WC_CMD='${env.SH_TOOLS}/otp_version ${OtpReleaseFN}'"
     String OtpRelease = readFile OtpReleaseFN
