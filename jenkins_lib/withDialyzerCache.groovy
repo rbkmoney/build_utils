91587cccf7f5dbb2b0ccf4ca3b838b22c8c588a0
Sergey Yelin <elinsn@gmail.com>
2020-07-07 13:02:38 +0300

    Add erlang service pipe (#76)
    * Add Erlang service pipe
    
    * Add option to run tests with container
    
    * Rename flag to testWithDependencies
    
    * Harden otp version filename (avoid clashes)
    
    * Add option to run tests in parallel
    
    * Rename call to run for Erlang pipes
    
    * run -> runPipe
    
    * Back to call
    
    * Fix dialyzer cache
    
    * Fix join
    
    * Rework parallel
    
    * Add info about pipline type to console
    
    * Change erlang pipe signature
    
    * New parallel
    
    * Do not force run stages in parallel
    
    * More on parallel
    
    * Add pipeline to parallel
    
    * Final cleanup
    
    * Rework parallel: add name
    
    * Fix parallel run
    
    * Return stages pack
    
    * Fix test logic
    
    * Change default for test with deps
    
    * Typo
    
    * Force check image
    
    * Move script dir setup to Jenkinsfile
    
    * Add fail fast to parallel run

---
+++ jenkins_lib/withDialyzerCache.groovy
@@ -0,0 +1,18 @@
+def call(Closure body) {
+    def withWsCache = load("${env.JENKINS_LIB}/withWsCache.groovy")
+    String OtpReleaseFN = "otp_version." + suffix()
+    sh "make wc_cmd WC_CMD='${env.SH_TOOLS}/otp_version ${OtpReleaseFN}'"
+    String OtpRelease = readFile OtpReleaseFN
+    def Plt = ["_build/default/rebar3_",OtpRelease,"_plt"].join("")
+    withWsCache(Plt, body)
+}
+
+static def suffix() {
+    def pool = ['a'..'z','A'..'Z',0..9].flatten()
+    Random rand = new Random(System.currentTimeMillis())
+
+    def passChars = (0..6).collect { pool[rand.nextInt(pool.size())] }
+    passChars.join("")
+}
+
+return this;
