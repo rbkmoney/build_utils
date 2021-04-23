9b9d92d354706b052339d30ea7c358fdf9fc0852
Ivan Panteleev <amalgamm@users.noreply.github.com>
2021-04-23 15:33:43 +0300

    Update pipeJavaServiceInsideDocker.groovy

--- jenkins_lib/pipeJavaServiceInsideDocker.groovy
+++ jenkins_lib/pipeJavaServiceInsideDocker.groovy
@@ -24,7 +24,7 @@ def call(String serviceName, String baseImageTag, String buildImageTag, String d
 
     // Start db if necessary.
 
-    def insideParams = ' -v /var/usr/docker.sock:/var/usr/docker.sock '
+    def insideParams = ' -v /var/usr/docker.sock:/var/run/docker.sock '
     // Run mvn and generate docker file
     runStage('Execute build container') {
         withMaven() {
