0c1101f0cc302ab0da5592fb163342cb98140a22
avcherkasov <shnurok14@ya.ru>
2021-08-27 18:36:16 +0300

    JD-573: little refactor

--- jenkins_lib/pipeJavaLibInsideDocker.groovy
+++ jenkins_lib/pipeJavaLibInsideDocker.groovy
@@ -1,10 +1,7 @@
 //Default pipeline for Java library
 def call(String mvnArgs = "", // arguments for mvn install in build container. For exmple: ' -DjvmArgs="-Xmx256m" '
-         String serviceBaseImageTag = "1d57d77a38eb7b351eca3c1a9a3e45ec441ed9aa", // https://github.com/rbkmoney/image-service-java
          String buildImageTag = "e26b9f98e2da13f570197ce62c8f6247bbf93108" // https://github.com/rbkmoney/image-build-java
 ) {
-    // service java image tag
-    env.BASE_IMAGE_TAG = serviceBaseImageTag
     // build container image tag
     env.BUILD_IMAGE_TAG = buildImageTag
 
@@ -21,7 +18,7 @@ def call(String mvnArgs = "", // arguments for mvn install in build container. F
     runStage('Execute build container') {
         withMaven() {
             buildContainer.inside() {
-                if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME.startsWith('epic/')) {
+                if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME.startsWith('epic')) {
                     withGPG() {
                         sh 'mvn deploy --batch-mode --settings $SETTINGS_XML ' + "${mvnArgs}" +
                                 ' -Dgpg.keyname="$GPG_KEYID" -Dgpg.passphrase="$GPG_PASSPHRASE" '
