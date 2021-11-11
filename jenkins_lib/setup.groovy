33fe256233edd6a091e846e3b235f06248a963c0
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-11-11 12:20:46 +0300

    ref: Refactor code and fix scoping

--- jenkins_lib/setup.groovy
+++ jenkins_lib/setup.groovy
@@ -1,9 +1,10 @@
-def call(modules, jenkins_lib = null, sh_tools = null) {
+def call(List modules, String jenkins_lib = "", String sh_tools = "") {
     env.JENKINS_LIB = jenkins_lib ?: env.JENKINS_LIB ?: "build_utils/jenkins_lib"
     env.SH_TOOLS = sh_tools ?: env.SH_TOOLS ?: "build_utils/sh"
     modules.each { mod ->
         if (!binding.hasVariable("${mod}")) {
-            evaluate("def ${mod} = load(\"${env.JENKINS_LIB}/${mod}.groovy\")")
+            file = "${env.JENKINS_LIB}/${mod}.groovy"
+            evaluate("${mod} = load(\"${file}\")")
         }
     }
 }
